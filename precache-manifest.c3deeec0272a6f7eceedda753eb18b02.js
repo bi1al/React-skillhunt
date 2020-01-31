caa551a6015cbe84ad7c6a794c8a89c93a87b08e
Bilal Amin khan <pukhtoon004@gmail.com>
2020-01-31 18:47:38 +0500

    Updates

---
+++ precache-manifest.c3deeec0272a6f7eceedda753eb18b02.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34d93572c3cdf0b08d39b8b3fedbe81f",
+    "url": "/React-skillhunt/index.html"
+  },
+  {
+    "revision": "cc35e9499a82a06cb87c",
+    "url": "/React-skillhunt/static/css/main.a5925e23.chunk.css"
+  },
+  {
+    "revision": "3303ab9c5ed39768e7a8",
+    "url": "/React-skillhunt/static/js/2.1850878b.chunk.js"
+  },
+  {
+    "revision": "cc35e9499a82a06cb87c",
+    "url": "/React-skillhunt/static/js/main.deff40e3.chunk.js"
+  },
+  {
+    "revision": "494f0780fce7a83b282f",
+    "url": "/React-skillhunt/static/js/runtime-main.19a0feab.js"
+  },
+  {
+    "revision": "36960386da1229909decb16cfbdd8a34",
+    "url": "/React-skillhunt/static/media/bg_2.36960386.png"
+  },
+  {
+    "revision": "5c54e7db1ace6de29b5eeab5b6c3271f",
+    "url": "/React-skillhunt/static/media/company-1.5c54e7db.jpg"
+  },
+  {
+    "revision": "83b75aed436e855936b13186468e90e7",
+    "url": "/React-skillhunt/static/media/company-2.83b75aed.jpg"
+  },
+  {
+    "revision": "99665fddd3159549ba44920df0bb4fd9",
+    "url": "/React-skillhunt/static/media/company-3.99665fdd.jpg"
+  },
+  {
+    "revision": "4b4537fb2e0d2cb22dd76b51eb43de18",
+    "url": "/React-skillhunt/static/media/company-4.4b4537fb.jpg"
+  },
+  {
+    "revision": "d14f069c1b411d12f95a6ee38f9e2a59",
+    "url": "/React-skillhunt/static/media/image_1.d14f069c.jpg"
+  },
+  {
+    "revision": "14403cb3ca2274b3b4795655d3427de2",
+    "url": "/React-skillhunt/static/media/image_2.14403cb3.jpg"
+  },
+  {
+    "revision": "4e18b4542e13cd8f71ba5e0c2ff04bc1",
+    "url": "/React-skillhunt/static/media/image_3.4e18b454.jpg"
+  },
+  {
+    "revision": "c9f8c66271b62f6c9be82016cad48476",
+    "url": "/React-skillhunt/static/media/image_4.c9f8c662.jpg"
+  },
+  {
+    "revision": "b5f00557708edc7ba6575a4532fd7379",
+    "url": "/React-skillhunt/static/media/image_5.b5f00557.jpg"
+  },
+  {
+    "revision": "b1088273bf9329efc45f9c0663168d38",
+    "url": "/React-skillhunt/static/media/image_7.b1088273.jpg"
+  },
+  {
+    "revision": "dfde74396689c32dfbf82bdbfb240e0d",
+    "url": "/React-skillhunt/static/media/image_8.dfde7439.jpg"
+  },
+  {
+    "revision": "bbcd5413ffcc46f7d9eef5fb36340659",
+    "url": "/React-skillhunt/static/media/person_1.bbcd5413.jpg"
+  },
+  {
+    "revision": "2cd3dcd36bb7c61c847ac770c2aff481",
+    "url": "/React-skillhunt/static/media/person_2.2cd3dcd3.jpg"
+  },
+  {
+    "revision": "514eee1b06ddf4a5692f7264d3da79aa",
+    "url": "/React-skillhunt/static/media/person_3.514eee1b.jpg"
+  },
+  {
+    "revision": "cfb95125014362aa10870999ff5d645d",
+    "url": "/React-skillhunt/static/media/person_4.cfb95125.jpg"
+  },
+  {
+    "revision": "fe2d5086de5476df5d52ad7d7a969698",
+    "url": "/React-skillhunt/static/media/person_5.fe2d5086.jpg"
+  },
+  {
+    "revision": "ec78a91f6ca666cb41c83fc5885fe8ca",
+    "url": "/React-skillhunt/static/media/person_6.ec78a91f.jpg"
+  }
+]);
